61b23e25a2f4980fdfb59bdac71786b94559fc80
Amaury Martiny <amaury.martiny@protonmail.com>
2020-11-06 11:52:45 +0100

    Add tm secp256k1

--- crypto/codec/tm.go
+++ crypto/codec/tm.go
@@ -3,18 +3,21 @@ package codec
 import (
 	tmcrypto "github.com/tendermint/tendermint/crypto"
 	tmed25519 "github.com/tendermint/tendermint/crypto/ed25519"
-	tmprotocrypto "github.com/tendermint/tendermint/proto/tendermint/crypto"
+	tmsecp256k1 "github.com/tendermint/tendermint/crypto/secp256k1"
 
 	"github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
-// FromTmPubKey converts TM's tmcrypto.PubKey toour own PubKey.
+// FromTmPubKey converts TM's tmcrypto.PubKey to our own PubKey.
 func FromTmPubKey(tmPk tmcrypto.PubKey) (cryptotypes.PubKey, error) {
 	switch tmPk := tmPk.(type) {
 	case tmed25519.PubKey:
 		return &ed25519.PubKey{Key: []byte(tmPk)}, nil
+	case tmsecp256k1.PubKey:
+		return &secp256k1.PubKey{Key: []byte(tmPk)}, nil
 	default:
 		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidType, "cannot convert %v to cryptotypes.PubKey", tmPk)
 	}
@@ -26,22 +29,9 @@ func ToTmPubKey(pk cryptotypes.PubKey) (tmcrypto.PubKey, error) {
 	switch pk := pk.(type) {
 	case *ed25519.PubKey:
 		return tmed25519.PubKey(pk.Key), nil
+	case *secp256k1.PubKey:
+		return tmsecp256k1.PubKey(pk.Key), nil
 	default:
 		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidType, "cannot convert %v to Tendermint public key", pk)
-	}
-
-}
-
-// ToTmPublicKey converts our own PubKey to TM's tmprotocrypto.PublicKey.
-func ToTmPublicKey(pk cryptotypes.PubKey) (tmprotocrypto.PublicKey, error) {
-	switch pk := pk.(type) {
-	case *ed25519.PubKey:
-		return tmprotocrypto.PublicKey{
-			Sum: &tmprotocrypto.PublicKey_Ed25519{
-				Ed25519: pk.Key,
-			},
-		}, nil
-	default:
-		return tmprotocrypto.PublicKey{}, sdkerrors.Wrapf(sdkerrors.ErrInvalidType, "cannot convert %v to Tendermint public key", pk)
 	}
 }
